4d32c96f58912fbc0f8c34f12aedfe121febc9d8
Anders Musikka <anders@andersmusikka.se>
2018-03-18 22:31:20 +0100

    Minimized savefile-lib

---
+++ savefile-derive/src/lib.rs
@@ -0,0 +1,8 @@
+#![feature(proc_macro)]
+extern crate proc_macro;
+
+#[proc_macro_derive(Savefile, attributes(versions, versions_as, default_val, default_fn))]
+pub fn savefile(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    panic!("Not implemented")
+}
+
